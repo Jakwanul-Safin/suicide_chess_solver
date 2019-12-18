387ede444b1c4950aca5261769b8bb0f909b9235
alexkhoma99 <ak4109@columbia.edu>
2019-12-18 13:57:06 -0500

    Minor comments removed

--- gametest.hs
+++ gametest.hs
@@ -5,12 +5,10 @@ import Data.List
 import Chess
 import Control.Parallel.Strategies(using, parList, rseq, rpar)
 
-pos = fromJust $ fromFEN "8/8/8/4n3/6q1/8/5K2/8 w - - 0 1"
--- negaMax 6 pt -> takes ~20 seconds, solves correctly.
+pos = fromJust $ fromFEN "8/8/8/4n3/6q1/8/5K2/8 w - - 0 1" -- Mate in 1
 pt = fromJust $ fromFEN "2b2b2/1p2p3/4k3/8/3qP3/r7/5P2/2n1K3 w - - 0 20" -- Mate in 3
 pt3 = fromJust $ fromFEN "8/5r2/8/5p2/6N1/3Q4/4K3/8 w - - 0 1" -- Mate in 3
 pt4 = fromJust $ fromFEN "8/5r2/8/5p2/6N1/3QP3/4K3/8 w - - 0 1" -- Mate in 4
--- negaMax 8 pt4 -> ~ 120 seconds.
 pt5 = fromJust $ fromFEN "8/8/8/8/4r2p/8/2P3K1/8 w - - 2 28" -- Mate in 2
 pt6 = fromJust $ fromFEN "8/1n3k2/4p3/7p/4r3/7K/2P5/3R4 w - - 2 28" -- Mate in 3
 
